811b0557eeb7f161410651bbf7c630dfe6004776
Osborn <110415101+asap-blip@users.noreply.github.com>
2024-02-24 20:50:06 +0300

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,8 @@
+# A prgram asking user for their name, age, location
+
+name = input('What is your name:')
+age = input('How old are you?')
+location = input('Where are you from?')
+
+print("Hello " + name + ", you are " + age + " years old and live in " + location +". Really nice to meet you")
+
