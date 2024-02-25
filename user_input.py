cb12baf78acc78e2d2f1e5a2f68712ca28239770
Osborn <110415101+asap-blip@users.noreply.github.com>
2024-02-25 13:35:30 +0300

    Update user_input.py
    Added some comments to the code

--- user_input.py
+++ user_input.py
@@ -1,8 +1,16 @@
-# A prgram asking user for their name, age, location
+# OSBORN NYAKARU
+# Power Learn Project Python Assignment Week 1
+
+# A program asking user for their name, age, location
+# Use the input() function to ask the user for their name and store it in a variable called "name", 
+# age and store it in a variable called "age, their location and store it in a variable called "location".
 
 name = input('What is your name:')
 age = input('How old are you?')
 location = input('Where are you from?')
+
+# Print out a personalized message using the user's name, age, and location. 
+# For example: "Hello [name], you are [age] years old and live in [location]."
 
 print("Hello " + name + ", you are " + age + " years old and live in " + location +". Really nice to meet you")
 
